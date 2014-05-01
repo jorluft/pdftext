f8be094718e584c1338d3f086e58245484eca8f4
Jordan Luft <jorluft@gmail.com>
2014-04-30 17:48:46 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pdftext::Application.config.secret_key_base = '2a177d3492c48ad04e941858205f6637b7ce979c6c7f71ee5f7ce255c7ffa317c77c45ee94ca72208eee78a9dc0f6b41ed86e47b0ca002d9a84f45c77ad82100'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pdftext::Application.config.session_store :cookie_store, key: '_pdftext_session'
