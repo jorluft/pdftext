92b74327dd06b0ad8b807de18a28796bc55e9a10
Jordan Luft <jorluft@gmail.com>
2014-05-02 23:44:10 -0700

    added devise and pins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Pdftext::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+#set this to actual host
+  config.action_mailer.default_url_options = { :host => 'http://pinteresting-commits.herokuapp.com/' }
 end
